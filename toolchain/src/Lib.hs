35d6034ee3b7b023e3c100eef7b53c87e9ef90e6
Hannu Hartikainen <hannu.hartikainen@gmail.com>
2017-08-05 11:52:43 +0300

    switch to megaparsec-6.0.2 and another tutorial

--- toolchain/src/Lib.hs
+++ toolchain/src/Lib.hs
@@ -2,9 +2,22 @@ module Lib
     ( assembler
     ) where
 
+import Data.Void
 import Text.Megaparsec
-import Text.Megaparsec.String
+import Text.Megaparsec.Char
+import Text.Megaparsec.Expr
 import Text.Printf
+import qualified Text.Megaparsec.Char.Lexer as L
+
+
+type Parser = Parsec Void String
+
+sc :: Parser ()
+sc = L.space space1 lineComment blockComment
+  where
+    lineComment  = L.skipLineComment "//"
+    blockComment = L.skipBlockComment "/*" "*/"
+
 
 assembler :: Parser String
 assembler = line
@@ -19,15 +32,20 @@ aInstr = do
     return $ printf "%016b" (read n :: Integer)
 
 cInstr :: Parser String
-cInstr = cDest <|> cJump
+cInstr = do
+    dest <- optional cDest
+    comp <- cComp
+    jump <- optional cJump
+    return $ comp
 
 cDest = do
     dest <- some (char 'A' <|> char 'M' <|> char 'D')
     char '='
     return $ dest
 
+cComp = some alphaNumChar
+
 cJump = do
-    some alphaNumChar
     char ';'
     jumpType <- some alphaNumChar
     return $ jumpType
