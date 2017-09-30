366f53cbe0c8315507b6dc96e315a9c8a1b8da7d
Hannu Hartikainen <hannu.hartikainen@gmail.com>
2017-09-30 14:27:36 +0300

    test script: save output

--- toolchain/test-asm.sh
+++ toolchain/test-asm.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
+stack build
+
+mkdir -p out
 for asm in ref/asm/*.asm
 do
-    stack exec assembler < $asm | diff -q - ref/asm/$(basename $asm .asm).hack
+    name=$(basename $asm .asm)
+    stack exec assembler < $asm > out/$name.hack
+    diff -q out/$name.hack ref/asm/$name.hack
 done
