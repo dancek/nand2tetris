d92877dae179d233d95d00b2fc87aa36cf35e587
Hannu Hartikainen <hannu.hartikainen@gmail.com>
2019-02-21 21:52:35 +0200

    VM push and pop base cases

---
+++ toolchain/src/AsmBackend.hs
@@ -0,0 +1,57 @@
+module AsmBackend where
+
+{-
+Generate assembly code from VM AST.
+-}
+
+import VMAST
+
+pushValue :: MemorySegment -> Integer -> [String]
+pushValue ms i = loadValue ms i ++ pushRegD
+
+-- Load value from given memory segment to the D register
+loadValue :: MemorySegment -> Integer -> [String]
+loadValue MConstant i = [
+  "@" ++ show i,
+  "D=A"]
+loadValue memseg i = [
+  "@" ++ segmentSymbol memseg,
+  "D=A",
+  "@" ++ show i,
+  "A=A+D",
+  "D=M"]
+
+segmentSymbol MLocal = "LCL"
+segmentSymbol MArgument = "ARG"
+segmentSymbol MThis = "THIS"
+segmentSymbol MThat = "THAT"
+
+-- Push register D content
+pushRegD :: [String]
+pushRegD = [
+  "@SP",
+  "A=M",
+  "M=D",
+  "@SP",
+  "M=M+1"]
+
+popValue :: MemorySegment -> Integer -> [String]
+popValue memseg i = [
+  -- addr = seg + i
+  "@" ++ segmentSymbol memseg,
+  "D=A",
+  "@" ++ show i,
+  "D=A+D",
+  -- *SP = addr
+  "@SP",
+  "A=M",
+  "M=D",
+  -- SP--
+  "@SP",
+  "M=M-1",
+  -- *addr = *SP, ie. *(SP+1) = *SP
+  "A=M",
+  "D=M",
+  "A=A+1",
+  "M=D"]
+
