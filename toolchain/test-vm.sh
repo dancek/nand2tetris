9db08cf8934bede43314d872c6f079556a71d54c
Hannu Hartikainen <hannu.hartikainen@gmail.com>
2019-02-26 21:42:02 +0200

    script also runs VM translator tests

---
+++ toolchain/test-vm.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+stack build || exit 1
+
+for f in ../07/**/*.vm; do
+  base=$(dirname $f)/$(basename $f .vm)
+  echo $base.asm
+  stack exec vm-translator < $f > $base.asm
+  ../../tools/CPUEmulator.sh $base.tst
+done
--- toolchain/translate-vm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-set -e
-
-stack build
-
-for f in ../07/**/*.vm; do
-  echo $f
-  stack exec vm-translator < $f > $(dirname $f)/$(basename $f .vm).asm
-done
