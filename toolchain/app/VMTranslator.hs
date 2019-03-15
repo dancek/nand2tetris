08aa62da07dd4fc8f90aebfd47258581a0e7aa88
Hannu Hartikainen <hannu.hartikainen@gmail.com>
2019-03-15 23:32:39 +0200

    handle file opening in haskell

--- toolchain/app/VMTranslator.hs
+++ toolchain/app/VMTranslator.hs
@@ -1,13 +1,31 @@
 module Main where
 
+import Data.List
 import System.IO
+import System.Environment
+import System.Directory
 import Text.Megaparsec
 
 import Lib
 
 main :: IO ()
 main = do
-    input <- getContents
+    args <- getArgs
+    case length args of
+        0 -> getContents >>= compile
+        1 -> compileFile $ head args
+
+compileFile :: String -> IO ()
+compileFile f = do
+    isFile <- doesFileExist f
+    if isFile then readFile f >>= compile
+    else do
+        allFiles <- listDirectory f
+        let files = filter (isSuffixOf ".vm") allFiles in
+            mapM_ compileFile $ map (\x -> f ++ "/" ++ x) files
+
+
+compile input =
     case vmTranslator input of
         Left e -> hPutStr stderr $ errorBundlePretty e
         Right r -> putStr r
