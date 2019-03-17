0ae69be5b55355b0b29b9e091fcf802f188df2f5
Hannu Hartikainen <hannu.hartikainen@gmail.com>
2019-03-17 23:08:40 +0200

    Output *.vm to *.asm, stdin to stdout

--- toolchain/app/VMTranslator.hs
+++ toolchain/app/VMTranslator.hs
@@ -12,20 +12,29 @@ main :: IO ()
 main = do
     args <- getArgs
     case length args of
-        0 -> getContents >>= compile
+        0 -> getContents >>= (compile Nothing)
         1 -> compileFile $ head args
 
 compileFile :: String -> IO ()
 compileFile f = do
     isFile <- doesFileExist f
-    if isFile then readFile f >>= compile
+    if isFile then readFile f >>= (compile $ Just f)
     else do
         allFiles <- listDirectory f
         let files = filter (isSuffixOf ".vm") allFiles in
-            mapM_ compileFile $ map (\x -> f ++ "/" ++ x) files
+            withCurrentDirectory f $ mapM_ compileFile files
 
-
-compile input =
+compile :: Maybe String -> String -> IO ()
+compile sourceFilename input =
     case vmTranslator input of
         Left e -> hPutStr stderr $ errorBundlePretty e
-        Right r -> putStr r
+        Right r -> case sourceFilename of
+            Just f -> let outfile = (getFilename f) in
+                putStrLn outfile >> writeFile outfile r
+            Nothing -> putStr r
+
+getFilename :: String -> String
+getFilename f =
+    if ".vm" `isSuffixOf` f then
+        take ((length f) - 3) f ++ ".asm"
+    else f ++ ".asm"
