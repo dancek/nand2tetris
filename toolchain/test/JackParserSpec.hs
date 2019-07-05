5b989e0f121ad54a63703f5546d806e3cdf8790d
Hannu Hartikainen <hannu.hartikainen@gmail.com>
2019-07-05 21:53:02 +0300

    Include parser input as quasiquoted raw string

--- toolchain/test/JackParserSpec.hs
+++ toolchain/test/JackParserSpec.hs
@@ -1,69 +1,78 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 module JackParserSpec where
 
 import Test.Hspec
 import Test.Hspec.Megaparsec
+
+import Text.RawString.QQ (r)
 
 import Text.Megaparsec (parse)
 import JackParser
 
 -- helper for running individual parsers with proper space consumer etc
 expectParse parser input expected =
-    parse (wrapParser parser) "" input
-        `shouldParse` expected
+  parse (wrapParser parser) "" input
+    `shouldParse` expected
 
 spec :: Spec
 spec = do
-    describe "class" $ do
-        it "empty body" $
-            expectParse jackClass "class Foo {\n}" $ JackClass "Foo" [] []
+  describe "class" $ do
+    it "empty body" $
+      expectParse jackClass "class Foo {}" $
+        JackClass "Foo" [] []
 
-        it "1 static var" $
-            expectParse jackClass
-                "class Foo {\n\
-                \    static int foo;\n\
-                \}" $
-                JackClass "Foo"
-                    [StaticDec (VarDec IntType ["foo"])]
-                    []
+    it "1 static var" $
+      expectParse jackClass
+        [r|
+          class Foo {
+            static int foo;
+          }
+        |] $ JackClass "Foo"
+              [StaticDec (VarDec IntType ["foo"])]
+              []
 
-        it "multi-declaration" $
-            expectParse jackClass
-                "class Foo {\n\
-                \    static int foo, bar, baz;\n\
-                \}" $
-                JackClass "Foo"
-                    [StaticDec (VarDec IntType ["foo", "bar", "baz"])]
-                    []
+    it "multi-declaration" $
+      expectParse jackClass
+        [r|
+          class Foo {
+            static int foo, bar, baz;
+          }
+        |] $ JackClass "Foo"
+              [StaticDec (VarDec IntType ["foo", "bar", "baz"])]
+              []
 
-        it "fields, class types" $
-            expectParse jackClass
-                "class Foo {\n\
-                \    field boolean bar;\n\
-                \    field AnotherClass baz;\n\
-                \}" $
-                JackClass "Foo"
-                    [ FieldDec (VarDec BooleanType ["bar"])
-                    , FieldDec (VarDec (ClassType "AnotherClass") ["baz"])
-                    ]
-                    []
+    it "fields, class types" $
+      expectParse jackClass
+        [r|
+          class Foo {
+            field boolean bar;
+            field AnotherClass baz;
+          }
+        |] $ JackClass "Foo"
+              [ FieldDec (VarDec BooleanType ["bar"])
+              , FieldDec (VarDec (ClassType "AnotherClass") ["baz"])
+              ]
+              []
 
-        it "empty subroutine" $
-            expectParse jackClass
-                "class Foo {\n\
-                \    method void noop() {\n\
-                \        return;\n\
-                \    }\n\
-                \}" $
-                JackClass "Foo"
-                    []
-                    [ SubroutineDec VoidType "noop" []
-                        (SubroutineBody [] [ReturnStatement])
-                    ]
+    it "empty subroutine" $
+      expectParse jackClass
+        [r|
+          class Foo {
+            method void noop() {
+              return;
+            }
+          }
+        |] $ JackClass "Foo"
+              []
+              [ SubroutineDec VoidType "noop" []
+                (SubroutineBody [] [ReturnStatement])
+              ]
 
 
-    describe "subroutine" $ do
-        it "no-op" $
-            expectParse subroutineDec
-                "method void noop() {return;}" $
-                SubroutineDec VoidType "noop" [] $
-                    SubroutineBody [] [ReturnStatement]
+  describe "subroutine" $ do
+    it "no-op" $
+      expectParse subroutineDec
+        "method void noop() {return;}" $
+        SubroutineDec VoidType "noop" [] $
+          SubroutineBody [] [ReturnStatement]
